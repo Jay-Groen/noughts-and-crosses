e3d2c98c43d5b816aecfb71ef37ebfe4ae32e542
Jay-Groen <groe0076@hz.nl>
2023-10-05 16:30:55 +0900

    starting point

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import HelloWorld from './HelloWorld.js';
-
-const greeting = new HelloWorld();
-console.log(greeting.greet());
-window.addEventListener('load', () => {
-  document.getElementById('content').innerText = greeting.greet();
-}
-);
